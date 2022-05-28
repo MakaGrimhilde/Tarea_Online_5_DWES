eeef4611e33e63ff1e19f6a5bba91de91155c20c
MakaGrimhilde <simtoclatro@gmail.com>
2022-05-28 13:37:15 +0200

    Sexto commit del proyecto. Añadidos comentarios al fichero controlador.php

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -2,51 +2,71 @@
 
 require "../modelo/modelo.php";
 
+/**
+ * Clase Controlador, mediante la cual se podrá acceder a las funciones de la clase Modelo
+ */
 class controlador{
 
     private $modelo;
 
+    /**
+     * Constructor de la clase Controlador, inicializa el objeto modelo
+     */
     public function __construct(){
 
-        $this->modelo = new modelo();
+        $this->modelo = new modelo(); 
     }
 
+    /**
+     * función que permite obtener un registro o varios de la base de datos por la petición GET
+     *
+     * @return void
+     */
     public function get(){
 
+        //si se recibe un parámetro GET 'id'
         if(isset($_GET['id'])){
 
-            $this->modelo->getId();
+            $this->modelo->getId(); //se llama a la función get por la id
             
-        } else {
+        } else { //si no, se accede a la función get global
 
-            $this->modelo->get();
+            $this->modelo->get(); 
         } 
 
     }
 
+    /**
+     * función que permite insertar un registro en la base de datos por medio de POST
+     *
+     * @return void
+     */
     public function post(){
 
         $this->modelo->post();
         
     }
 
+    /**
+     * función que permite actualizar un registro concreto a modo de petición PUT
+     *
+     * @return void
+     */
     public function put(){
 
         $this->modelo->put();
         
     }
 
+    /**
+     * función que permite eliminar un registro mediante su id (petición DELETE)
+     *
+     * @return void
+     */
     public function delete(){
 
         $this->modelo->delete();
     }
 }
-
-
- 
-
-    
-     
-    
 
 ?>
